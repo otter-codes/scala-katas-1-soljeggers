1842309eda74d480ab864996f27b874cf9c077fd
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-01 15:17:00 +0000

    Add countOdds.scala

---
+++ src/main/scala/example/countOdds.scala
@@ -0,0 +1,17 @@
+package example
+
+import scala.io.StdIn
+
+object countOdds extends App {
+
+  val num = StdIn.readLine("What is your final number? ").toInt
+
+  val EvenNumbers = List.range(0, num ,2)
+  print(EvenNumbers)
+
+// take number from console
+// using number from console
+  // create a list of numbers i.e. 1 to 10
+  // get me a list of ints where they're even
+
+}
